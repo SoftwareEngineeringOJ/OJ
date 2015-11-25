dc79dd17c29c6f3602f24e76b0ed552d066c57aa
addf400 <addf400@foxmail.com>
2015-11-25 12:23:50 +0800

    整理文件

--- scrapytest/scrapytest/spiders/spider.py
+++
@@ -1,8 +0,0 @@
-from scrapy.contrib.spiders import CrawlSpider
-
-class spyyyyy(CrawlSpider):
-    name = "ppp"
-    start_urls = ["http://www.bnuoj.com/v3/problem.php"]
-    def parse(self,response):
-        print response.body
-
