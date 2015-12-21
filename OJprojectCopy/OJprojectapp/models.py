3eb10fe54422827457ffec2c6a148dbc3a524477
addf400 <addf400@foxmail.com>
2015-12-21 15:03:21 +0800

    Change models.py

--- OJprojectCopy/OJprojectapp/models.py
+++ OJprojectCopy/OJprojectapp/models.py
@@ -3,28 +3,63 @@ from django.db import models
 from django.contrib import admin
 # Create your models here.
 
+class problemslist(models.Model):
+    OJ = models.CharField(max_length=20)
+    SID = models.CharField(max_length=20)
+    title = models.CharField(max_length=50)
+    ratio = models.CharField(max_length=50)
+    time_limit = models.CharField(max_length=15)
+    mem_limit = models.CharField(max_length=15)
+    description = models.TextField()
+    inputs = models.TextField()
+    output = models.TextField()
+    sample_input = models.TextField()
+    sample_output = models.TextField()
+    hint = models.TextField()
+    pic = models.CharField(max_length=300)
+
+class group_status(models.Model):
+    groupID = models.CharField(max_length=30)
+    groupname = models.CharField(max_length=50)
+    result = models.CharField(max_length=50)
+    competedate = models.DateTimeField()
+    codes = models.TextField()
+    is_codes_private = models.BooleanField()
+    is_group_private = models.BooleanField()
+    submit_time = models.DateTimeField() 
+
+class user_status(models.Model):
+    problemID = models.CharField(max_length=15)
+    problemTitle = models.CharField(max_length=35)
+    problemSID = models.CharField(max_length=15)
+    contestID = models.CharField(max_length=10)
+    userID = models.CharField(max_length=20)
+    username = models.CharField(max_length=50)
+    OJ = models.CharField(max_length=10)
+    result = models.CharField(max_length=30)
+    memory = models.CharField(max_length=15)
+    time = models.CharField(max_length=15)
+    language = models.CharField(max_length=10)
+    code = models.TextField()
+    submit_time = models.DateTimeField()
+    is_code_private = models.BooleanField()
+    is_user_private = models.BooleanField()
+
 class user(models.Model):
-    userID = models.CharField(max_length=30)
     username = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
     signature = models.TextField(max_length=200)
     school = models.CharField(max_length=50)
     email = models.EmailField(max_length=50)
     register_time= models.DateTimeField()
-
-class status(models.Model):
-    runID = models.CharField(max_length=15)
-    username = models.CharField(max_length=30)
-    OJ = models.CharField(max_length=20)
-    result = models.CharField(max_length=30)
-    memory = models.CharField(max_length=15)
-    time = models.CharField(max_length=15)
-    language = models.CharField(max_length=10)
-    code_file = models.CharField(max_length=50)
-    submit_time = models.DateTimeField()
-    userID = models.CharField(max_length=30)
-    contestID = models.CharField(max_length=10)
-    isprivate = models.BooleanField()
+    answered = models.CharField(max_length=50)
+    wrong = models.CharField(max_length=50)
+    right = models.CharField(max_length=50) 
+    contest_number = models.CharField(max_length=50)
+    contest_wrong = models.CharField(max_length=50)
+    contest_right = models.CharField(max_length=50)
+    problems = models.ManyToManyField(user_status)
+    contests = models.ManyToManyField(group_status)
 
 class contest(models.Model):
     title = models.CharField(max_length = 50) #比赛标题
@@ -39,59 +74,40 @@ class contest(models.Model):
     begintime = models.DateTimeField() #开始时间
     endtime = models.DateTimeField() #结束时间
 
-class problemslist(models.Model):
-    OJ = models.CharField(max_length=20)
-    SID = models.CharField(max_length=20)
-    title = models.CharField(max_length=50)
-    ratio = models.CharField(max_length=50)
-    source = models.CharField(max_length=50)
-
-class problems(models.Model):
-    problemID = models.CharField(max_length=15)
-    OJ = models.CharField(max_length=20)
-    SID = models.CharField(max_length=20)
-    title = models.CharField(max_length=50)
-    time_limit = models.CharField(max_length=15)
-    mem_limit = models.CharField(max_length=15)
-    description = models.TextField()
-    inputs = models.TextField()
-    output = models.TextField()
-    sample_input = models.TextField()
-    sample_output = models.TextField(max_length=250)
-    hint = models.TextField()
-    pics = models.TextField()
-
-class userdetail(models.Model): 
-    username = models.CharField(max_length=50)
-    precision = models.CharField(max_length=10)
-    number = models.CharField(max_length=10)
-    algorithm = models.CharField(max_length=10)
-    speed = models.CharField(max_length=10)
-    sumrank = models.CharField(max_length=50)
-    problems = models.ManyToManyField(problems)
-
-class groupdetail(models.Model):
+class group(models.Model):
     groupname = models.CharField(max_length=50)
-    sumrank = models.CharField(max_length=50)
-    competedate = models.DateTimeField()
+    groupmembers = models.ManyToManyField(user)
+    register_time= models.DateTimeField()
+
+
+class discussion(models.Model):
+    userID = models.CharField(max_length=20)
+    username = models.CharField(max_length=50)
+    problemID = models.CharField(max_length=15)
+    problemTitle = models.CharField(max_length=35)
+    speak = models.TextField(max_length=300)
+    like = models.TextField(max_length=50)
+    dislike = models.TextField(max_length=50)
+    submit_time = models.DateTimeField()
+
 
 class useradmin(admin.ModelAdmin):
-    list_display = ('username','username')
-class statusadmin(admin.ModelAdmin):
-    list_display = ('runID','userID')
+    list_display = ('username','register_time')
+class groupadmin(admin.ModelAdmin):
+    list_display = ('groupname','register_time')
+class user_statusadmin(admin.ModelAdmin):
+    list_display = ('username','submit_time')
+class group_statusadmin(admin.ModelAdmin):
+    list_display = ('groupname','submit_time')
 class problemslistadmin(admin.ModelAdmin):
-    list_display = ('OJ','source')
-class userdetailadmin(admin.ModelAdmin):
-    list_display = ('username','sumrank')
-class groupdetailadmin(admin.ModelAdmin):
-    list_display = ('groupname','sumrank')
-class problemsadmin(admin.ModelAdmin):
     list_display = ('OJ','title')
+class discussionadmin(admin.ModelAdmin):
+    list_display = ('problemTitle','submit_time')
 
 
 admin.site.register(user,useradmin)
-admin.site.register(status,statusadmin)
+admin.site.register(group,groupadmin)
+admin.site.register(user_status,user_statusadmin)
+admin.site.register(group_status,group_statusadmin)
 admin.site.register(problemslist,problemslistadmin)
-admin.site.register(problems,problemsadmin)
-admin.site.register(userdetail,userdetailadmin)
-admin.site.register(groupdetail,groupdetailadmin)
+admin.site.register(discussion,discussionadmin)
